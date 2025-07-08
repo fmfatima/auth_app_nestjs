ee0dc38cdbf0d81bb473df2c79bf5c3d3ee7ecb9
fmfatima <fm.fatimamalik005@gmail.com>
2025-07-08 22:37:44 +0500

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // ✅ Load .env globally
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URI'),
+      }),
+    }),
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
+
+
+
+
+
+
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { AuthModule } from './auth/auth.module';
+
+// @Module({
+//   imports: [AuthModule],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
